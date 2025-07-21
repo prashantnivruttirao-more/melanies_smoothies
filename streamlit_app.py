04caf8fc9d5cf94ddf047602d6ba44dee930ef9e
prashantnivruttirao-more <prashantnivruttirao.more@barclays.com>
2025-07-21 13:11:06 +0530

    Update streamlit_app.py
    deleted last extra part

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,9 +55,4 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered:'+ name_on_order, icon="✅")
-# new section to display smoothie 
-import requests
-smoothiefroot_response = requests.get("https://FRUITYVICE.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response.json())
 
-st_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
